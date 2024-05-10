a85e54a5b2ac4c5b1f7adcb313375b6c30b1ad50
nitter1 <danielroger2001@gmail.com>
2024-05-10 00:24:23 -0300

    Correção no envio de emails.

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -7,28 +7,28 @@ jQuery(document).ready(function($) {
     var action = f.attr('action');
 
     if (!action) {
-      action = 'https://api.staticforms.xyz/submit'; // URL da API para enviar o formulário
+      action = 'https://api.staticforms.xyz/submit';
     }
 
     $.ajax({
       type: "POST",
       url: action,
       data: str,
-      success: function(msg) {
-        if (msg == 'OK') {
+      success: function(response) {
+        if (response && response.success) {
           $("#sendmessage").addClass("show");
           $("#errormessage").removeClass("show");
           $('.contactForm').find("input, textarea").val("");
         } else {
           $("#sendmessage").removeClass("show");
           $("#errormessage").addClass("show");
-          $('#errormessage').html(msg); // Exibir mensagem de erro retornada pela API
+          $('#errormessage').html("Erro ao enviar o formulário. Por favor, tente novamente.");
         }
       },
       error: function(xhr, status, error) {
         $("#sendmessage").removeClass("show");
         $("#errormessage").addClass("show");
-        $('#errormessage').html("Erro ao enviar o formulário. Por favor, tente novamente."); // Mensagem de erro genérica
+        $('#errormessage').html("Erro ao enviar o formulário. Por favor, tente novamente.");
       }
     });
 
